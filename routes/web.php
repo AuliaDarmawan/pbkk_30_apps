04762c6ba6008b8de95f6353a62f768ed8b7e633
Aulia_Darmawan <pureldarma@gmail.com>
2024-03-17 10:57:59 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+ 
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+Route::get('/welcome', function () {
+    return 'Hello World';
+});
